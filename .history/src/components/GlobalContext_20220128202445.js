b9e94ff9c8e699cde59f7d863e496f7d5d623cba
Oskar Dąbrowski <74384399+oskardabrowski@users.noreply.github.com>
2022-03-05 12:25:53 +0100

    Delete .history directory

--- .history/src/components/GlobalContext_20220128202445.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const GlobalContext = () => {
-  return <div></div>;
-};
-
-export default GlobalContext;
--- .history/src/components/api_20220112130735.js
+++
@@ -1,7 +0,0 @@
-const apiday = 'http://api.openweathermap.org/data/2.5/weather';
-const api7 = 'http://api.openweathermap.org/data/2.5/forecast';
-const lang = 'pl';
-const key = 'faf68383b7364b3650fc15f93c575c50';
-const key7 = '1e273549948a637fecb6b6a41947883f';
-
-export default const api = {apiday, api7, lang, key, key7}
